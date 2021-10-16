7e9398b5a4e8992149cefd2abbf9fc58a40f088d
Nick Wu <wypwgx@163.com>
2021-10-16 21:25:09 +0800

    add  class  ArrayStack.

---
+++ src/ArrayStack.java
@@ -0,0 +1,9 @@
+public class ArrayStack {
+    private char[] stk;
+    private int length;
+    
+    public ArrayStack(int max){
+        stk = new char[max];
+        this.length = max;
+    }
+}
